9beff3caa3068b7e6f62e843a76c2cd2b2cb52e7
Anderson <Andexito902@gmail.com>
2026-06-26 17:50:26 -0500

    Fase GREEN

--- src/incidentes/AsignadorTicketsService.java
+++ src/incidentes/AsignadorTicketsService.java
@@ -3,6 +3,6 @@ package incidentes;
 public class AsignadorTicketsService {
 
     public double calcularPrioridad(int impacto, int gravedad) {
-        return 0.0;
+        return (impacto * 0.6) + (gravedad * 0.4);
     }
 }
