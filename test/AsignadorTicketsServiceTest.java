f1fbe43f59bbcc02d4745b63adc4a3a84e3d7d59
Anderson <Andexito902@gmail.com>
2026-06-26 17:07:06 -0500

    docs: definicion de objetivos y diseño inicial de la estructura de pruebas unitarias

---
+++ test/AsignadorTicketsServiceTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AsignadorTicketsServiceTest {
+
+    @Test
+    public void testCalcularPrioridadMaximaDeberiaDarCinco() {
+
+        //primera prueba bajo el enfoque TDD
+        fail("Prueba fallida intencionalmente: Aún no existe la lógica de negocio (Fase RED)");
+    }
+
+}
