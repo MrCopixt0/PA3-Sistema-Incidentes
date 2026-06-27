f19defd78a82372cb53d37f0b982847bf9618498
MrCopixt0 <Andexito902@gmail.com>
2026-06-26 19:05:36 -0500

    Kata1: Fase REFACTOR - Extraccion de logica de validacion a un metodo privado

--- src/incidentes/AsignadorTicketsService.java
+++ src/incidentes/AsignadorTicketsService.java
@@ -4,9 +4,13 @@ public class AsignadorTicketsService {
 
     public double calcularPrioridad(int impacto, int gravedad) {
 
-        if (impacto < 1 || impacto > 5 || gravedad < 1 || gravedad > 5){
+        validarRangos(impacto, gravedad); //se llama al nuevo metodo
+        return (impacto * 0.6) + (gravedad * 0.4);
+    }
+
+    private void validarRangos(int impacto, int gravedad) {
+        if (impacto < 1 || impacto > 5 || gravedad < 1 || gravedad > 5) {
             throw new IllegalArgumentException("El impacto y la gravedad deben estar entre 1 y 5");
         }
-        return (impacto * 0.6) + (gravedad * 0.4);
     }
 }
