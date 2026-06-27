2ed0826a8f3be6e96bc478623a460641cdde93e1
MrCopixt0 <Andexito902@gmail.com>
2026-06-26 19:00:12 -0500

    Kata 1: Fase Green - Implementacion de control de excepciones para rangos invalidos

--- src/incidentes/AsignadorTicketsService.java
+++ src/incidentes/AsignadorTicketsService.java
@@ -3,6 +3,10 @@ package incidentes;
 public class AsignadorTicketsService {
 
     public double calcularPrioridad(int impacto, int gravedad) {
+
+        if (impacto < 1 || impacto > 5 || gravedad < 1 || gravedad > 5){
+            throw new IllegalArgumentException("El impacto y la gravedad deben estar entre 1 y 5");
+        }
         return (impacto * 0.6) + (gravedad * 0.4);
     }
 }
