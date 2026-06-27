06fff6e6ba94618292fbfa8f54265f421f073a13
MrCopixt0 <Andexito902@gmail.com>
2026-06-27 02:42:14 -0500

    Actualización ORM para clase -Agente.java-

--- src/incidentes/Agente.java
+++ src/incidentes/Agente.java
@@ -1,10 +1,26 @@
 package incidentes;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "agentes")
 
 public class Agente {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+
+    @Column(name = "nombre", nullable = false)
     private String nombre;
+
+    @Column(name = "rol", nullable = false)
     private String rol;
+
+    @Column(name = "carga_trabajo")
     private int cargaTrabajo;
+
+    public Agente() {} //constructor vacio requerido por el ORM para instanciar objetos
 
     public Agente(String nombre, String rol, int cargaTrabajo) {
 
@@ -13,8 +29,14 @@ public class Agente {
         this.cargaTrabajo = cargaTrabajo;
     }
 
+    //getters y setters - escritos manual xd
+
+    public Long getId() {return id;}
     public String getNombre() {return nombre;}
     public String getRol() {return rol;}
-    public int getCargaTrabajo(){return cargaTrabajo;}
+    public int getCargaTrabajo() {return cargaTrabajo;}
 
+    public void setCargaTrabajo(int cargaTrabajo) {
+        this.cargaTrabajo = cargaTrabajo;
+    }
 }
